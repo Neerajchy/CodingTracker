8baebda37d4ae3ebb5ca45225c82fb289593a01c
NEERAJ CHAUDHARY <NEERAJ CHAUDHARY@DESKTOP-1C50MBK>
2024-04-02 14:19:40 +0545

    we have many messages

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world:nirajkumar");
+            Console.WriteLine("hello world:nirajkumar"); Console.WriteLine("hello world:nirajkumar");
+            Console.WriteLine("hello world:nirajkumar"); Console.WriteLine("hello world:nirajkumar");
+            Console.WriteLine("hello world:nirajkumar"); Console.WriteLine("hello world:nirajkumar"); Console.WriteLine("hello world:nirajkumar");
         }
     }
 }
